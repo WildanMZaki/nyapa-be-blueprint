adbf3e87cb91ec0108d8ac121a11ced2dd1724af
Wildan M Zaki <wildanmzaki7@gmail.com>
2025-02-21 22:01:12 +0700

    refactor: make ModelProvider definition easier

--- src/schemas/models.ts
+++ src/schemas/models.ts
@@ -1,15 +1,33 @@
 import { Provider } from '@nestjs/common';
-import { Connection } from 'mongoose';
+import { Connection, Document, Model, Schema } from 'mongoose';
 import { Contact, ContactSchema } from 'src/schemas/contact.schema';
 import { TenantService } from 'src/tenant/tenant.service';
 
-export const ContactModelProvider: Provider = {
-  provide: 'CONTACT_MODEL',
-  useFactory: (tenantService: TenantService) => {
-    const connection: Connection = tenantService.getConnection();
-    return connection.model<Contact>('Contact', ContactSchema);
-  },
-  inject: [TenantService],
+export type ModelProvider<Document> = Provider & {
+  provide: string;
+  useFactory: (tenantService: TenantService) => Model<Document>;
+  inject: [typeof TenantService];
 };
 
-export const models: Array<Provider> = [ContactModelProvider];
+export function createModelProvider<T>(
+  name: string,
+  schema: Schema<T>,
+): ModelProvider<T> {
+  const prefix: string = '';
+  const suffix: string = '_MODEL';
+  return {
+    provide: prefix + name.toUpperCase() + suffix,
+    useFactory: (tenantService: TenantService): Model<T> => {
+      const connection: Connection = tenantService.getConnection();
+      return connection.model<T>(name, schema);
+    },
+    inject: [TenantService],
+  };
+}
+
+export const ContactModelProvider = createModelProvider<Contact>(
+  'Contact',
+  ContactSchema,
+);
+
+export const models: Array<ModelProvider<Document>> = [ContactModelProvider];
